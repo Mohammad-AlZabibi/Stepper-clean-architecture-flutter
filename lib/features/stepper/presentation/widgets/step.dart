b378465d9ff97f877931f5dca3dc43b3fa76bcbf
Mohammad Alzabibi <mohammad.alzabibi@gmail.com>
2022-10-11 22:18:01 +0300

    Enhance stepper content style

--- lib/features/stepper/presentation/widgets/step.dart
+++ lib/features/stepper/presentation/widgets/step.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 Step step({required String title, String? subtitle, required String description, required int index, required int currentStep}) {
   return Step(
-    title: Text(title,style: TextStyle(color: currentStep == index ? Colors.black : null),),
+    title: Padding(
+      padding: const EdgeInsets.only(top:16.0),
+      child: Text(title,style: TextStyle(color: currentStep == index ? Colors.black : null),),
+    ),
     subtitle: Text(subtitle ?? ""),
     content: Column(
       children: <Widget>[
