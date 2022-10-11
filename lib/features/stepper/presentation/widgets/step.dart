d119d437d7933ba1f0ff368cc625a9b1e2ac5706
Mohammad Alzabibi <mohammad.alzabibi@gmail.com>
2022-10-11 22:14:56 +0300

    - Change current step title color on active

--- lib/features/stepper/presentation/widgets/step.dart
+++ lib/features/stepper/presentation/widgets/step.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Step step({required String title, String? subtitle, required String description, required int index, required int currentStep}) {
   return Step(
-    title: Text(title),
+    title: Text(title,style: TextStyle(color: currentStep == index ? Colors.black : null),),
     subtitle: Text(subtitle ?? ""),
     content: Column(
       children: <Widget>[
